209da343f489d9ac242e4f0db784e5b565a238a2
Maanas777 <maanasmohan77@gmail.com>
2023-11-11 11:10:55 +0530

    dist

--- Clientside/src/main.tsx
+++ Clientside/src/main.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { Provider } from "react-redux";
-// import { BrowserRouter } from 'react-router-dom';
-import store from "./redux/store.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from './redux/store.tsx';
 // import { HashRouter as Router } from 'react-router-dom';
-import {} from "react-router-dom";
+import { } from 'react-router-dom';
 
 // Use React.Fragment or a div to wrap the entire application
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
