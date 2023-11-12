821d2fa11aa346bca454ccdc4113b54c57bc5e74
Maanas777 <maanasmohan77@gmail.com>
2023-11-12 22:17:33 +0530

    js2

--- Serverside/src/dist/server.js
+++ Serverside/src/dist/server.js
@@ -38,10 +38,10 @@ const corsOptions = {
 app.use((0, cors_1.default)(corsOptions));
 
 
-app.use(express_1.static(path.join(__dirname,"../../../Clientside/dist")));
+app.use(express_1.static(path_1.join(__dirname,"../../../Clientside/dist")));
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname,"../../../Clientside/dist/index.html"));
+    res.sendFile(path_1.join(__dirname,"../../../Clientside/dist/index.html"));
   });
 
 
